33662cb71389b8d000d015920d5afa7e4c56313d
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-20 13:06:38 -0600

    Diseño de NothingSelectedView concepto de vistas (views)

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { Typography } from '@mui/material';
 import { JorunalLayout } from '../layout/JorunalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JorunalLayout>
 
-      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, impedit suscipit! Assumenda, tempora. Nisi voluptas quos, accusantium architecto cum mollitia quae quia, explicabo, unde nihil soluta at eveniet voluptatem alias?</Typography>
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, impedit suscipit! Assumenda, tempora. Nisi voluptas quos, accusantium architecto cum mollitia quae quia, explicabo, unde nihil soluta at eveniet voluptatem alias?</Typography> */}
 
-      {/* NothingSelected */}
+      <NothingSelectedView />
 
       {/* NoteView */}
 
