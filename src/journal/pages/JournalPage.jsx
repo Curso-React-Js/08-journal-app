d5569535c5282687381d5a446921eedd0aa9bace
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-20 13:33:25 -0600

    Diseño de view NoteView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,5 @@
 import { JorunalLayout } from '../layout/JorunalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
@@ -7,9 +7,9 @@ export const JournalPage = () => {
 
       {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, impedit suscipit! Assumenda, tempora. Nisi voluptas quos, accusantium architecto cum mollitia quae quia, explicabo, unde nihil soluta at eveniet voluptatem alias?</Typography> */}
 
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
 
-      {/* NoteView */}
+      <NoteView />
 
     </JorunalLayout>
   );
