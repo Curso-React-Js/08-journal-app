4784f8feff628b968f0f7f0552fd36907f15c587
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-23 20:08:59 -0600

    Manejo de formulario con thunks, useForm e implementaciones basico

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,16 +1,40 @@
 import { Link as RouterLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 // xs => es igual a las pantallas (pantallas pequeñas) md
 export const LoginPage = () => {
+
+  const dispatch = useDispatch();
+
+  const { email, password, onInputChange } = useForm({
+    email: 'test@test.com',
+    password: '12345678'
+  });
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    
+    console.log({ email, password });
+    dispatch( checkingAuthentication() );
+  }
+  
+  const onGoogleSignIn = () => {
+    console.log('onGoogleSignIn');
+    dispatch( startGoogleSignIn() );
+  }
+
   return (
     <AuthLayout title="Login">
 
-      <form>
+      <form onSubmit={ onSubmit }>
         <Grid container>
 
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -18,7 +42,10 @@ export const LoginPage = () => {
               label="Correo" 
               type="email"
               placeholder="correo@google.com"
-              fullWidth />
+              fullWidth
+              name="email"
+              value={ email }
+              onChange={ onInputChange } />
           </Grid>
 
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -26,12 +53,16 @@ export const LoginPage = () => {
               label="Contraseña" 
               type="password"
               placeholder="*********"
-              fullWidth />
+              fullWidth
+              name="password"
+              value={ password }
+              onChange={ onInputChange } />
           </Grid>
 
           <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={ 12 } sm={ 6 }>
-              <Button 
+              <Button
+                type="submit"
                 variant="contained"
                 fullWidth>Login</Button>
             </Grid>
@@ -39,7 +70,8 @@ export const LoginPage = () => {
             <Grid item xs={ 12 } sm={ 6 }>
               <Button 
                 variant="contained"
-                fullWidth>
+                fullWidth
+                onClick={ onGoogleSignIn }>
                   <Google />
                   <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
