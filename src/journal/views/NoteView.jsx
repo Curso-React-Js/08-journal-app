f0f8f5d9a6cfd136cc89f77e5c6b6ad0e182c1a9
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-25 13:40:16 -0600

    Activar nota actual (mostrar y cambiar)

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,9 +1,21 @@
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
 
+import { useForm } from '../../hooks';
 import { ImageGallery } from './ImageGallery';
 
 export const NoteView = () => {
+
+  const { active: note } = useSelector( state => state.journal );
+  const { body, title, date, onInputChange, formState } = useForm( note );
+
+  const dateString = useMemo(() => {
+    const newDate = new Date( date );
+    return newDate.toUTCString();
+  }, [ date ]);
+
   return (
     <Grid
       className="animate__animated animate__fadeIn animate__faster"
@@ -14,7 +26,7 @@ export const NoteView = () => {
       sx={{ mb: 1 }}>
 
       <Grid item>
-        <Typography fontSize={ 39 } fontWeight="light">28 de septiembre, 2023</Typography>
+        <Typography fontSize={ 39 } fontWeight="light">{ dateString }</Typography>
       </Grid>
 
       <Grid item>
@@ -31,7 +43,10 @@ export const NoteView = () => {
           fullWidth 
           placeholder="Ingrese un título" 
           label="Título"
-          sx={{ border: 'none', mb: 1 }} />
+          sx={{ border: 'none', mb: 1 }}
+          name="title"
+          value={ title }
+          onChange={ onInputChange } />
 
         <TextField 
           type="text"
@@ -39,7 +54,10 @@ export const NoteView = () => {
           fullWidth
           multiline
           placeholder="¿Qué sucedió en el día de hoy?"
-          minRows={ 5 } />
+          minRows={ 5 }
+          name="body"
+          value={ body }
+          onChange={ onInputChange } />
       </Grid>
 
       {/* Image gallery */}
