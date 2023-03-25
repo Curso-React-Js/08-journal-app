22ad4bf7ab0f054c7a3f6986806ee4dfafe6c9ec
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-24 19:54:41 -0600

    Formulario de registro

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -3,12 +3,27 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+const formData = {
+  email: 'test@test.com',
+  password: '12345678',
+  displayName: 'Angel Flores'
+}
 
 export const RegisterPage = () => {
+
+  const { displayName, email, password, onInputChange, formState } = useForm(formData);
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <AuthLayout title="Crear cuenta">
 
-      <form>
+      <form onSubmit={ onSubmit }>
         <Grid container>
 
         <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -16,7 +31,10 @@ export const RegisterPage = () => {
               label="Nombre completo" 
               type="text"
               placeholder="Steve Jobs"
-              fullWidth />
+              fullWidth
+              name="displayName"
+              value={ displayName }
+              onChange={ onInputChange } />
           </Grid>
 
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -24,7 +42,10 @@ export const RegisterPage = () => {
               label="Correo" 
               type="email"
               placeholder="correo@google.com"
-              fullWidth />
+              fullWidth
+              name="email"
+              value={ email }
+              onChange={ onInputChange }  />
           </Grid>
 
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -32,12 +53,16 @@ export const RegisterPage = () => {
               label="Contraseña" 
               type="password"
               placeholder="*********"
-              fullWidth />
+              fullWidth
+              name="password"
+              value={ password }
+              onChange={ onInputChange }  />
           </Grid>
 
           <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={ 12 }>
-              <Button 
+              <Button
+                type="submit"
                 variant="contained"
                 fullWidth>Crear cuenta</Button>
             </Grid>
