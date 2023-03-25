9e3c5eaf507a56b35982e20b77e5da6ebd251031
Luis Angel <angelfgdeveloper@gmail.com>
2023-03-25 13:27:04 -0600

    Dar clic en in item y activar la informacion seleccionada

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,8 +1,17 @@
 import { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { TurnedInNot } from '@mui/icons-material';
 
-export const SideBarItem = ({ title, body, id }) => {
+import { setActiveNote } from '../../store/journal';
+
+export const SideBarItem = ({ title = '', body, id, date, imageUrls = [] }) => {
+
+  const dispatch = useDispatch();
+
+  const onClickNote = () => {
+    dispatch( setActiveNote({ title, body, id, date, imageUrls }) );
+  }
 
   const newTitle = useMemo(() => {
     return title.length > 17 
@@ -12,7 +21,7 @@ export const SideBarItem = ({ title, body, id }) => {
 
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={ onClickNote }>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
